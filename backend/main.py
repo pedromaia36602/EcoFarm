a946d8f36e4560a4f8d4db4ebd7ac659f7f0d0f9
Pedro Maia <115093035+pedromaia36602@users.noreply.github.com>
2025-11-14 23:04:02 -0300

    Refactor main.py for improved readability

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,14 @@
 import serial
-from flask_cors import CORS   # ✅ importe o CORS
+from flask_cors import CORS  
 import sqlite3
 import time
 from flask import Flask, jsonify
 
-# === CONFIGURAÇÕES ===
-PORTA_SERIAL = 'COM3'  # troque conforme sua porta
+
+PORTA_SERIAL = 'COM3'  
 BAUDRATE = 9600
 
-# === BANCO DE DADOS ===
+
 conn = sqlite3.connect('dados_arduino.db', check_same_thread=False)
 cursor = conn.cursor()
 cursor.execute('''
@@ -21,14 +21,14 @@ CREATE TABLE IF NOT EXISTS registros (
 ''')
 conn.commit()
 
-# === LEITURA SERIAL ===
+
 arduino = serial.Serial(PORTA_SERIAL, BAUDRATE, timeout=1)
 time.sleep(2)
 print("✅ Conectado ao Arduino na porta", PORTA_SERIAL)
 
-# === SERVIDOR FLASK ===
+
 app = Flask(__name__)
-CORS(app)  # ✅ habilita CORS para todas as rotas
+CORS(app)  
 
 @app.route("/dados")
 def get_dados():
@@ -42,7 +42,7 @@ def salvar_no_banco(umidade, bomba):
     cursor.execute("INSERT INTO registros (umidade, bomba_acionada) VALUES (?, ?)", (umidade, bomba))
     conn.commit()
 
-# === LOOP DE LEITURA SERIAL ===
+
 def loop_serial():
     while True:
             linha = arduino.readline().decode('utf-8').strip()
@@ -58,6 +58,6 @@ def loop_serial():
 
 if __name__ == "__main__":
     from threading import Thread
-    # roda o servidor Flask e a leitura serial ao mesmo tempo
+
     Thread(target=loop_serial, daemon=True).start()
     app.run(host="0.0.0.0", port=5000)
